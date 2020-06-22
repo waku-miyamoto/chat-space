f5bada4ddbd0f8b39a5e987ed9133d32c8eac5f3
waku-miyamoto <67049564+waku-miyamoto@users.noreply.github.com>
2020-06-22 17:37:36 +0900

    Revert "mini edit"
    This reverts commit 4e3b7df55d026a0c7259a6a7bf27288649ff9d90.

--- db/migrate/20200622065046_create_groups.rb
+++ db/migrate/20200622065046_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
-      t.string :name
+      t.string :title
       t.string :user_id
       t.timestamps
     end
