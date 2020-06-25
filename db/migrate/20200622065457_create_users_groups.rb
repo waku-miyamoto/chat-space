bf005cebc8bdc629d97c4d9da16a123c58249306
waku-miyamoto <67049564+waku-miyamoto@users.noreply.github.com>
2020-06-25 22:36:00 +0900

    group create

--- db/migrate/20200622065457_create_users_groups.rb
+++ db/migrate/20200622065457_create_users_groups.rb
@@ -1,8 +1,8 @@
 class CreateUsersGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :users_groups do |t|
-      t.integer :user_id, null: false, foreign_key: true
-      t.integer :group_id, null: false, foreign_key: true
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
